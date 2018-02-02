ec537054601efc485eb1bc1eada240036083cefa
Aji Nur Laksono <ajilakson@gmail.com>
2018-02-02 18:13:10 +0700

    no message

---
+++ app/src/main/java/com/oji/hellotoast/MainActivity.java
@@ -0,0 +1,35 @@
+package com.oji.hellotoast;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    int count;
+    TextView showCount;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showCount = (TextView) findViewById(R.id.show_count);
+
+    }
+
+    public void showToast(View view) {
+
+        Context context = getApplicationContext();
+        Toast toast = Toast.makeText(context, "Hello Toast", Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+    count++;
+        if(showCount != null){
+        showCount.setText(Integer.toString(count));
+
+        }
+    }
+}
